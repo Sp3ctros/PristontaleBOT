25c99589aad69bbfda7af28ec672eb43f2650388
JoaovcLisboa <90469235+Sp3ctros@users.noreply.github.com>
2023-07-14 15:17:00 -0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -2,10 +2,10 @@ import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium import webdriver
 import discord
+import asyncio
 
 
 file = open('Token.txt','r')
@@ -14,20 +14,23 @@ file.close()
 
 
 class Bosses:
-    def __init__(self, nome, periodo, tempo_final,tempo_restante):
+    def __init__(self, nome, boss_cycle, end_time,time_left):
         self.nome = nome
-        self.periodo = periodo
-        self.tempo_final = tempo_final
-        self.tempo_restante = tempo_restante
-#atualiza o tempo restante para o boss nascer
+        self.boss_cycle = boss_cycle
+        self.end_time = end_time
+        self.time_left = time_left
+        
+#update the left time
+
 def att_Timer():
     for m in lista_bosses:
         tempo_atual = datetime.datetime.now()
-        if tempo_atual < m.tempo_final:
-            m.tempo_restante = m.tempo_final - datetime.datetime.now()
+        if tempo_atual < m.end_time:
+            m.time_left = m.end_time - datetime.datetime.now()
         else:
-            m.tempo_final = m.tempo_final + datetime.timedelta(hours=m.periodo)
-#Pega os valores de referencia dos timers e cria um objeto da classe Bosses para cada um dos boss
+            m.end_time = m.end_time + datetime.timedelta(hours=m.boss_cycle)
+
+#scrap the web for the time left and create objects for every boss
 def extrair_da_web():
     # Configurar o caminho para o driver do navegador
     webdriver_service = Service('chromedriver.exe')
@@ -40,67 +43,69 @@ def extrair_da_web():
     url = 'https://babel-pteu.netlify.app/'
     driver.get(url)
 
-    time.sleep(3)
+    asyncio.sleep(3)
     # Extrair a informação desejada (nome dos bosses e o tempo restante para nascer)
-    element = driver.find_elements(By.CLASS_NAME, "count-down")
-    element2 = driver.find_elements(By.CLASS_NAME, "countdown-boss-name")
+    web_time_left = driver.find_elements(By.CLASS_NAME, "count-down")
+    bosses_name = driver.find_elements(By.CLASS_NAME, "countdown-boss-name")
     #Itera cada um dos bosses e cria um objeto na classe Bosses para cada um.
     tempo_atual = datetime.datetime.now()
-    for c, i in enumerate(element):
+    for c, i in enumerate(web_time_left):
         hor = str(i.text)
         if len(hor) == 8:
             hora = float(hor[0:2])
             min = float(hor[3:5])
             sec = float(hor[6:7])
-        tempo_final = tempo_atual + datetime.timedelta(hours=hora, minutes=min, seconds=sec)
-        boss = Bosses(nome=element2[c].text,periodo=Periodo[c],tempo_final=tempo_final, tempo_restante=i.text)
+        end_time = tempo_atual + datetime.timedelta(hours=hora, minutes=min, seconds=sec)
+        boss = Bosses(nome=bosses_name[c].text,boss_cycle=boss_cycle[c],end_time=end_time, time_left=i.text)
         lista_bosses.append(boss)
     driver.quit()
 
+    
+    
 lista_bosses = list()
-#Período de nascimento de cada um dos bosses
-Periodo = [1,2,2,3,3,3,3,4,4,4,6,6,6,12,12]
+#bosses cycle
+boss_cycle = [1,2,2,3,3,3,3,4,4,4,6,6,6,12,12]
 
-#Permissões necessárias do bot
+#discord intents
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-#detecta quando o bot é iniciado
+#detects when the bot is runing
 @client.event
 async def on_ready():
     print(f'{client.user} está online')
 
-#Detecta quando tem alguma mensagem no chat e verifica se é algum comando.
+#detects a message on the chat and verifies if its a command or not
 @client.event  
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content == '!boss':
-        #Adiciona no chat a tabela com o tempo dos bosses
+        #insert on the chat the bosses timer.
         embed=discord.Embed(title="Timer dos bosses", color=0xFF5733)
         for d in lista_bosses:
-            embed.add_field(name = '', value = f'**➤{d.nome} **: {str(d.tempo_restante)[:7]}', inline=False)
+            embed.add_field(name = '', value = f'**➤{d.nome} **: {str(d.time_left)[:7]}', inline=False)
         embed.set_footer(text = f'Ultima atualização: {str(datetime.datetime.now())[:16]}')
         embed.set_image(url="https://static.subagames.com/PT1/images/guide/charinfo/priest1.jpg")
         msg = await message.channel.send(embed=embed)
-        #Atualiza a tabela dos tempos uma certa quantidade de vezes
+        #refresh the timers a range of times
         for c in range(1000):
             att_Timer()
             embed=discord.Embed(title="Timer dos bosses", color=0xFF5733)
             for d in lista_bosses:
-                    embed.add_field(name = '', value = f'**➤{d.nome} **: {str(d.tempo_restante)[:7]}', inline=False)
+                    embed.add_field(name = '', value = f'**➤{d.nome} **: {str(d.time_left)[:7]}', inline=False)
             embed.set_footer(text = f'Ultima atualização: {str(datetime.datetime.now())[:16]}\nJoão Lisboa')
             embed.set_image(url="https://static.subagames.com/PT1/images/guide/charinfo/priest1.jpg")
             await msg.edit(embed=embed)
-            await asyncio.sleep(4)
+            asyncio.sleep(4)
         embed = discord.Embed(title="Timer Finalizado:", color=0xFF5733)
         await msg.edit(embed=embed)
 
-    #Atualiza os valores de referencia para os timers.
+    #refresh the reference values.
     if message.content == '!upboss':
         extrair_da_web()
 
-#EXTRAIR O TIMER DOS BOSSES E INICIAR O BOT
+#extract the bosses timer and starts the bot
 extrair_da_web()
 client.run(TOKEN)
